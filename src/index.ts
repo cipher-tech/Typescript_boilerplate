a8e0944496c83d9c35a53bdc5f27e7955100a3da
cipher-tech <nickchibuikem@gmail.com>
2023-09-15 16:32:44 +0100

    added index file

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import http from 'http';
+import { Express } from 'express';
+import "dotenv/config";
+
+import { envValidatorSchema } from './utils/validators';
+import Env from './utils/env';
+import Logger from './config/logger';
+// import { connectDB } from './config/database';
+import App from './config/express';
+import { AppEnv } from './utils/enums';
+
+async function main(App: (...args: any[]) => Express) {
+
+    // run the following three before initializing App function
+    await Env.validateEnv(envValidatorSchema);
+    //   await connectDB();
+    //   await configureSessionStore();
+
+    const logger = new Logger(App.name);
+    // global.logger = logger;
+    logger.info("LKJKJKJk")
+    const app = App();
+
+    const PORT = Env.get<number>('PORT') || 8080;
+    const NODE_ENV = Env.get<string>('NODE_ENV');
+
+    NODE_ENV !== AppEnv.PRODUCTION &&
+        app.listen(PORT, () => {
+            const NODE_ENV = Env.get<string>('NODE_ENV');
+
+            NODE_ENV !== AppEnv.PRODUCTION && console.log(`[server]: Server is running at http://localhost:${ PORT }`);
+        });
+
+}
+
+main(App);
