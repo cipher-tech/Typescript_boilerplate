28ba7c1c320c16a049b422ad5f900a98fb56a436
cipher-tech <nickchibuikem@gmail.com>
2023-09-17 00:27:59 +0100

    added defaults to env config

--- src/config/env/index.ts
+++ src/config/env/index.ts
@@ -1,7 +1,13 @@
+import path from 'path';
 import development from './development';
 import test from './test';
 
+const defaults = {
+  ROOT: path.normalize(`${__dirname}/..`),
+  SERVICE_NAME: "Roigour_OS_API"
+};
+
 export default {
-  development,
-  test,
+  development: Object.assign(defaults, development),
+  test: Object.assign(defaults, test)
 }[process.env._NODE_ENV || 'development'];
